e60c9b7b0426ea3e89cb61159d5c4e311c244030
Paweł Banach <1054126+pawlobanano@users.noreply.github.com>
2023-12-01 19:47:25 +0100

    fix: run benchmark for func processEmailDomainsConcurrently in parallel
    docs: replace old benchmark results with new screenshots

--- customerimporter/customerimporter_benchmark_test.go
+++ customerimporter/customerimporter_benchmark_test.go
@@ -22,23 +22,26 @@ func BenchmarkProcessEmailDomainsConcurrently(b *testing.B) {
 	for _, filePath := range filePaths {
 		b.Run(fmt.Sprintf("File: %s", filePath), func(b *testing.B) {
 			for _, parallelism := range []int{1, 2, 4, 8, 12} {
-				b.SetParallelism(parallelism)
-
 				b.Run(fmt.Sprintf("CPU core parallelism: %d", parallelism), func(b *testing.B) {
-					for i := 0; i < b.N; i++ {
-						file, err := os.Open(filePath)
-						if err != nil {
-							b.Fatal(err)
-						}
-						defer file.Close()
+					b.SetParallelism(parallelism)
+					b.ResetTimer()
 
-						reader, err := createCSVfileReader(log, config, file)
-						if err != nil {
-							b.Fatal(err)
-						}
+					b.RunParallel(func(pb *testing.PB) {
+						for pb.Next() {
+							file, err := os.Open(filePath)
+							if err != nil {
+								b.Fatal(err)
+							}
+							defer file.Close()
 
-						_ = processEmailDomainsConcurrently(log, config, reader)
-					}
+							reader, err := createCSVfileReader(log, config, file)
+							if err != nil {
+								b.Fatal(err)
+							}
+
+							_ = processEmailDomainsConcurrently(log, config, reader)
+						}
+					})
 				})
 			}
 		})
